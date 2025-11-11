f2d12caf4deae9019af27ced08e1308ae33222ad
112360102 <161789203+112360102@users.noreply.github.com>
2025-11-12 00:44:09 +0800

    Add files via upload

---
+++ ch2/sample2_12.java
@@ -0,0 +1,14 @@
+public class Sample2_12
+{
+    public static void main(string[] args)
+    {
+        int i , j;
+        for(i = 0 ; i < 5 ; i++)
+        {
+            for(j = 0 ; j < 3 ; j++)
+            {
+                System.out.println("i圈" + i + "j圈" + j);
+            }
+        }
+    }
+}
